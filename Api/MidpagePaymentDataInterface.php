d191c37249eecbe7ed69fd8a656386d37c636b99
Andrii Adamchuk <andrii@h1.nl>
2026-03-11 23:57:27 +0200

    BILLIINK-123 fix setup:upgrade error

--- Api/MidpagePaymentDataInterface.php
+++ Api/MidpagePaymentDataInterface.php
@@ -2,21 +2,32 @@
 
 namespace Billink\Billink\Api;
 
-use Magento\Quote\Api\Data\AddressInterface;
-use Magento\Quote\Api\Data\PaymentInterface;
-
 interface MidpagePaymentDataInterface
 {
+    /**
+     * @param int $cartId
+     * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
+     * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
+     * @return \Billink\Billink\Api\MidpageResultDataInterface $object
+     */
     public function savePaymentInformationAndPlaceOrder(
         int $cartId,
-        PaymentInterface $paymentMethod,
-        ?AddressInterface $billingAddress = null
+        \Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
+        ?\Magento\Quote\Api\Data\AddressInterface $billingAddress = null
     ): MidpageResultDataInterface;
 
+    /**
+     * @param string $cartId
+     * @param string $email
+     * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
+     * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
+     *
+     * @return \Billink\Billink\Api\MidpageResultDataInterface $object
+     */
     public function saveGuestPaymentInformationAndPlaceOrder(
         string $cartId,
         string $email,
-        PaymentInterface $paymentMethod,
-        ?AddressInterface $billingAddress = null
+        \Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
+        ?\Magento\Quote\Api\Data\AddressInterface $billingAddress = null
     ): MidpageResultDataInterface;
 }
